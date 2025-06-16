5d818c985b79ce4e84bf5e19b7c6f81ce52bfa57
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-06-16 22:21:55 +0900

    메인 색깔에 따라 텍스트 색상 수정 로직 추가

--- src/pages/Mobile/BoothAccount.tsx
+++ src/pages/Mobile/BoothAccount.tsx
@@ -61,6 +61,17 @@ function BoothAccount() {
     navigate("/post");
   };
 
+  const getTextColor = (bgColor: string) => {
+    const hex = bgColor.replace("#", "");
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
+
+    return luminance < 128 ? "white" : "black";
+  };
+
   useEffect(() => {
     const isWideScreen = window.innerWidth <= 500;
 
@@ -113,7 +124,10 @@ function BoothAccount() {
             <button
               onClick={handleClick}
               className={style.NutoButton}
-              style={{ backgroundColor: selectedBooth.mainColor }}
+              style={{
+                color: getTextColor(selectedBooth.mainColor),
+                backgroundColor: selectedBooth.mainColor,
+              }}
             >
               누토 남기기
             </button>
