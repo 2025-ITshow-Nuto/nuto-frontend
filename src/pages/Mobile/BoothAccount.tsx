d43e298e8665206e078f11273a3ddbf8a1925f92
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-06-16 23:31:14 +0900

    글자 색 계산 함수 로직 수정

--- src/pages/Mobile/BoothAccount.tsx
+++ src/pages/Mobile/BoothAccount.tsx
@@ -62,14 +62,13 @@ function BoothAccount() {
   };
 
   const getTextColor = (bgColor: string) => {
-    const hex = bgColor.replace("#", "");
-    const r = parseInt(hex.substring(0, 2), 16);
-    const g = parseInt(hex.substring(2, 4), 16);
-    const b = parseInt(hex.substring(4, 6), 16);
-
-    const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
-
-    return luminance < 128 ? "white" : "black";
+    const c = bgColor.substring(1);
+    const rgb = parseInt(c, 16);
+    const r = (rgb >> 16) & 0xff;
+    const g = (rgb >> 8) & 0xff;
+    const b = (rgb >> 0) & 0xff;
+    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return luma < 127.5 ? "white" : "black";
   };
 
   useEffect(() => {
